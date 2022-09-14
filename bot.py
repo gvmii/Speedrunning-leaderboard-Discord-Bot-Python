adae2b765d87d56ca3b23dcfb67457b3d1ad3e92
Gumi <clashroyaleixo@gmail.com>
2022-09-14 15:42:08 +0200

    Make a poggers command!!!
    YESSS

--- bot.py
+++ bot.py
@@ -86,30 +86,57 @@ async def ping(ctx):
     await ctx.send("pong")
 
 
-async def write_list(a_list):
-    async with aiofiles.open("users.json", "w") as f:
-        await f.write(json.dumps(a_list))
+async def write_to_file(data, file):
+    async with aiofiles.open(file, "w") as f:
+        await f.write(json.dumps(data))
         print("Done writing JSON data into .json file")
 
 
-async def read_list():
-    # for reading also binary mode is important
-    async with aiofiles.open("users.json", "r") as fp:
-        n_list = json.loads(await fp.read())
-        print(n_list)
-    return n_list
+async def read_dict(file):
+    async with aiofiles.open(file, "r") as f:
+        loaded_dict = json.loads(await f.read())
+        with_int_keys = {int(key): value for key, value in loaded_dict.items()}
+    return with_int_keys
+
+
+async def read_list(file):
+    async with aiofiles.open(file, "r") as f:
+        loaded_list = json.loads(await f.read())
+    return loaded_list
 
 
 @bot.command()
 async def register(ctx):
-    existing_users = await read_list()
+    existing_users = await read_list('users.json')
     user_id = ctx.author.id
     if user_id not in existing_users:
         existing_users.append(user_id)
-        await write_list(existing_users)
+        await write_to_file(existing_users, 'users.json')
         await ctx.send('Registered successfully')
     else:
         await ctx.send("You're already registered")
+
+
+@bot.command()
+async def favorite_song(ctx, song):
+    loaded_users = await read_list('user_songs.json')
+    user_id = ctx.author.id
+    # if user_id in loaded_users:
+
+    song_to_write = {user_id: song}
+    loaded_users.update(song_to_write)
+    print(loaded_users)
+    await write_to_file(loaded_users, 'user_songs.json')
+
+
+@bot.command()
+async def register_score(ctx, mode: str, time: int):
+    user_id = ctx.author.id
+    loaded_file = await read_dict('times.json')
+    writing_template = {user_id: {"AnyPercent": time}}
+    print(loaded_file)
+    loaded_file.update(writing_template)
+    await write_dictionary(loaded_file, 'times.json')
 
 
 bot.run(os.getenv("TOKEN"))
