bba4696960cf7d0a566880722e69af259e3900f5
Gumi <clashroyaleixo@gmail.com>
2022-09-15 03:54:01 +0200

    Clean up code, add helper functions, add personal best command

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import asyncio
 import json
 import os
 import sys
-import nextcord
-import aiohttp
+
 import aiofiles
-from rich import print, console
+import discord
+import nextcord
 from dotenv import load_dotenv
-from nextcord.ext import commands, tasks
+from nextcord.ext import commands
+from rich import print, console
+from datetime import datetime
 
 intents = nextcord.Intents.default()
 intents.message_content = True
@@ -22,19 +24,7 @@ console = console.Console()
 load_dotenv()
 
 
-class CloseButton(nextcord.ui.View):
-    def __init__(self, message: nextcord.Message):
-        super().__init__()
-        self.message = message
-
-    @nextcord.ui.button(label="🗑️", style=nextcord.ButtonStyle.red)
-    async def close_button(
-            self, button: nextcord.Button, interaction: nextcord.Interaction
-    ):
-        await self.message.delete()
-
-
-async def readConfig():
+async def read_config():
     async with aiofiles.open("config.json", mode="r", encoding="utf8") as jsonfile:
         contents = await jsonfile.read()
     config = json.loads(contents)
@@ -46,9 +36,9 @@ async def readConfig():
 # On ready, do this.
 @bot.event
 async def on_ready():
-    config = await readConfig()
+    config = await read_config()
     if not config["channel_id"]:
-        config = await readConfig()
+        config = await read_config()
     try:
         channel_id = int(config["channel_id"])
         console.log(f"Channel ID Set to: {channel_id}.")
@@ -59,21 +49,6 @@ async def on_ready():
     print(f"Logged in as {bot.user}.")
     channel = bot.get_channel(channel_id)
     console.log(f"Channel #{channel.name} ({channel.id}) [green]found[/green].")
-
-    # You have to make the message a variable so the ctx.edit() can use it. Without it, it doesn't have the correct
-    # context.
-    message = await channel.send(
-        embed=nextcord.Embed(
-            title="The bot has been enabled.", description="Welcome!", color=0x008B02
-        )
-    )
-    await asyncio.sleep(5)
-    embed = nextcord.Embed(
-        title="Command List:", description="!prioq \n !start", color=0x5300EB
-    ).set_footer(
-        text="Remember that you can always invoke the command list using !help"
-    )
-    await message.edit(embed=embed, delete_after=5)
 
 
 # @bot.event
@@ -105,6 +80,15 @@ async def read_list(file):
     return loaded_list
 
 
+async def validate_time(time):
+    # This might be slow because try excepts are slow in Python, but whatever.
+    # TODO: Make this a bit more efficient
+    try:
+        datetime.strptime(time, '%H:%M:%S.%f')
+    except ValueError:
+        print('invalid time')
+
+
 @bot.command()
 async def register(ctx):
     existing_users = await read_list('users.json')
@@ -130,13 +114,25 @@ async def favorite_song(ctx, song):
 
 
 @bot.command()
-async def register_score(ctx, mode: str, time: int):
+async def register_score(ctx, mode: str, time: str):
     user_id = ctx.author.id
     loaded_file = await read_dict('times.json')
-    writing_template = {user_id: {"AnyPercent": time}}
+    writing_template = {user_id: {mode: time}}
     print(loaded_file)
     loaded_file.update(writing_template)
     await write_to_file(loaded_file, 'times.json')
+
+
+@bot.command()
+async def personal_best(ctx, mode: str, user: discord.Member = None):
+    user_id = ctx.author.id
+    loaded_file = await read_dict('times.json')
+
+    user = user or ctx.author
+
+    if user.id in loaded_file:
+        pb = loaded_file[user_id].get(mode)
+        await ctx.send(f"{user.mention}'s {mode} time is {pb}")
 
 
 bot.run(os.getenv("TOKEN"))
