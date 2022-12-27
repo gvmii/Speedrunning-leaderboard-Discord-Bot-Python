b4dfb582f448d43685efe8d31e8443fc1f01374f
Amber <chawker374@gmail.com>
2022-12-27 14:08:44 -0800

    made setup.py readable

--- setup.py
+++ setup.py
@@ -1,37 +1,39 @@
 import os
 import json
-import subprocess
 
+#installs requirements, in case u don't have venv
 def install_requirements():
     print("Installing requirements")
     os.system("pip install -r requirements.txt")
 
 
-#creates config.json in the data directory
-
+#Creates data/ & creates and fills data/config.json
 def create_config():
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
     print("config.json not found. Creating it now")
     with open('data/config.json','w') as f:
         f.write('{}')
 
-def setup():
-    install_requirements()
-
-    if not os.path.exists('data'):
-        os.makedirs('data')
-
     data = {}
 
-    create_config()
     data["channel_id"] = input("What channel id would you like to use? ")
 
     with open('data/config.json','w') as f:
         f.write(json.dumps(data))
 
+#creates .env file if you don't have one, and fills in the token
+def create_dotenv():
     if not os.path.exists('.env'):
         token = input("What bot token would you like to use? ")
         with open('.env','w') as env:
             env.write(f'TOKEN="{token}"')
+
+def setup():
+    install_requirements()
+    create_config()
+    create_dotenv()
 
     input("Setup finished. Press enter to exit.")
     exit()
